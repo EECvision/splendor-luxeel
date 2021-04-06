efdba6a326913683f2771edf530b68e797494d0f
Emmanuel <eec.studies@gmail.com>
2021-04-06 21:28:32 +0100

    fixed ui issue

--- client/src/pages/collection/Menu.jsx
+++ client/src/pages/collection/Menu.jsx
@@ -7,7 +7,7 @@ const Menu = ({ title, items }) => {
   return (
     <>
       <div className="pt-28 w-full flex flex-col items-center justify-center px-2 md:px-0">
-        <div className="w-auto fixed z- left-4 top-20 bg-blue-600 px-4 py-1 rounded-full mb-2">
+        <div className="w-auto absolute left-4 top-20 bg-blue-600 px-4 py-1 rounded-full mb-2">
           <div className="text-sm text-white uppercase">{title}</div>
         </div>
           <div className="w-full max-w-screen-xl flex flex-wrap items-center justify-ceter lg:justify-start">
